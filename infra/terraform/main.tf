9b95bc4f74f2a2bad29f131eeecb71a7d5fc1cc6
Ubuntu <ubuntu@ip-10-0-10-206.ap-south-1.compute.internal>
2026-08-08 19:32:28 +0000

    Remove unused commented CloudFront configuration

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -150,55 +150,6 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   })
 }
 
-#resource "aws_cloudfront_distribution" "frontend_distribution" {
-#  enabled             = true
-#  default_root_object = "index.html"
-#  price_class         = var.cloudfront_price_class
-#
-#  origin {
-#    domain_name = aws_s3_bucket_website_configuration.frontend_bucket_website.website_endpoint
-#    origin_id   = "S3-Frontend-Origin"
-#
-#    custom_origin_config {
-#      http_port              = 80
-#      https_port             = 443
-#      origin_protocol_policy = "http-only"
-#      origin_ssl_protocols   = ["TLSv1.2"]
-#    }
-#  }
-#
-#  default_cache_behavior {
-#    allowed_methods  = ["GET", "HEAD"]
-#    cached_methods   = ["GET", "HEAD"]
-#    target_origin_id = "S3-Frontend-Origin"
-#
-#    forwarded_values {
-#      query_string = false
-#      cookies {
-#        forward = "none"
-#      }
-#    }
-#
-#    viewer_protocol_policy = "redirect-to-https"
-#  }
-#
-#  viewer_certificate {
-#    cloudfront_default_certificate = true
-#  }
-#
-#  restrictions {
-#    geo_restriction {
-#      restriction_type = "none"
-#    }
-#  }
-#
-#  tags = {
-#    Name = "FrontendCDN"
-# }
-
-#  depends_on = [aws_s3_bucket_policy.frontend_bucket_policy]
-#}
-
 resource "aws_iam_role" "presign_lambda_role" {
   name = "DevOps-Accelerator-Presign-Lambda-Role"
   assume_role_policy = jsonencode({
